b2ec673a574df3a8fe3d1a84d3abe9c32fecbed2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-12-18 17:09:47 +0530

    ✔️ crud functions are up

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,31 @@ import { Todo } from "@/types/todos.types";
 
 export default function Index() {
     const [_todos, setTodos] = useState<Todo[]>(todos.sort((a, b) => a.id - b.id));
+    const [text, setText] = useState<string>('');
+
+    const addTodos = () => {
+        if (text.trim()) {
+            const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+            setTodos([..._todos, { id: newId, text, completed: false }]);
+            setText('');
+        }
+    };
+
+    const toggleTodo = (id: number) => {
+        setTodos(
+            _todos.map((todo) => {
+                if (todo.id === id) {
+                    return { ...todo, completed: !todo.completed };
+                }
+                return todo;
+            })
+        )
+    }
+
+    const removeTodo = (id: number) => {
+        setTodos(_todos.filter((todo) => todo.id !== id));
+    };
+
     return (
         <View
             style={{
