e226c865b416049836a52d96090cf40576becda7
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-12-18 17:00:38 +0530

    🪟  added safe area view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,13 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+    return (
+        <SafeAreaProvider>
+            <Stack>
+                <Stack.Screen name="index" />
+            </Stack>
+        </SafeAreaProvider>
+    );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
+import React,{useState} from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Edit app/index.tsx to edit this screen.</Text>
+        </View>
+    );
 }
