878096259123a1b81887b0acc2bdd25506be7bff
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-12-18 16:57:30 +0530

    🫙 added demo todos

---
+++ data/todos.ts
@@ -0,0 +1,52 @@
+export const todos = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true,
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false,
+    },
+    {
+        id: 3,
+        text: "Clean the house",
+        completed: false,
+    },
+    {
+        id: 4,
+        text: "Write a blog post",
+        completed: false,
+    },
+    {
+        id: 5,
+        text: "Take a walk",
+        completed: false,
+    },
+    {
+        id: 6,
+        text: "Call a friend",
+        completed: false,
+    },
+    {
+        id: 7,
+        text: "Read a book",
+        completed: false,
+    },
+    {
+        id: 8,
+        text: "Do laundry",
+        completed: false,
+    },
+    {
+        id: 9,
+        text: "Go to the gym",
+        completed: false,
+    },
+    {
+        id: 10,
+        text: "Cook a meal",
+        completed: false,
+    },
+];
